a7a1a53738659a9967d1299cf920c5c09a330698
Adam Steinbach <31391830+steinbacha@users.noreply.github.com>
2022-06-17 10:53:35 -0500

    send file

--- myApp.js
+++ myApp.js
@@ -3,8 +3,10 @@ let express = require('express');
 let app = express();
 
 app.get('/', function(req, res) {
-    res.send("Hello Express");
+    res.sendFile(__dirname + '/views/index.html');
 });
+
+
 
 
 
